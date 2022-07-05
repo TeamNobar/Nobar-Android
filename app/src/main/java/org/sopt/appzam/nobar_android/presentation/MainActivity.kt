39ef975b167f4103ac1c9317a74b825fe1a3454d
권용민 <briandr@naver.com>
2022-07-05 17:54:35 +0900

    add dependencies

---
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.appzam.nobar_android.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.appzam.nobar_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
