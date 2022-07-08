bd8230c9cb2ade73e1d0c817d076783ef3b1da8c
hyeonjeong Park <py0429@ewhain.net>
2022-07-08 15:57:17 +0900

    #7 homeView layout - dataBinding으로 설정

---
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/home/HomeFragment.kt
@@ -0,0 +1,32 @@
+package org.sopt.appzam.nobar_android.presentation.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.sopt.appzam.nobar_android.R
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package org.sopt.appzam.nobar_android.presentation.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
