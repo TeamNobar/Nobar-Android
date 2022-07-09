3510d88285f80f95fc13f4716bef97aa7e2fa4ad
hyeonjeong Park <py0429@ewhain.net>
2022-07-09 10:42:56 +0900

    충돌

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/MainActivity.kt
@@ -1,11 +1,12 @@
 package org.sopt.appzam.nobar_android.presentation
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.appzam.nobar_android.R
-import org.sopt.appzam.nobar_android.databinding.ActivityHomeBinding
+import org.sopt.appzam.nobar_android.databinding.ActivityMainBinding
 import org.sopt.appzam.nobar_android.presentation.base.BaseActivity
 
-class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
