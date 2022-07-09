adc8f83ca503f49c90508a0f1d1ecb5aa11d8fcd
hyeonjeong Park <py0429@ewhain.net>
2022-07-09 10:30:59 +0900

    변경사항 적용

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/HomeActivity.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/HomeActivity.kt
@@ -1,12 +1,11 @@
 package org.sopt.appzam.nobar_android.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.appzam.nobar_android.R
-import org.sopt.appzam.nobar_android.databinding.ActivityMainBinding
+import org.sopt.appzam.nobar_android.databinding.ActivityHomeBinding
 import org.sopt.appzam.nobar_android.presentation.base.BaseActivity
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
