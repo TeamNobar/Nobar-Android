9cebac1e64033b8821306d9742a6702275144b95
hyeonjeong Park <py0429@ewhain.net>
2022-07-11 20:11:36 +0900

    #12 home - nobar_pick_recipe recyclerview

--- app/src/main/java/org/sopt/appzam/nobar_android/data/HomeLaterRecipeData.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/data/HomeLaterRecipeData.kt
@@ -1,6 +1,6 @@
 package org.sopt.appzam.nobar_android.data
 
-data class LaterRecipeData(
+data class HomeLaterRecipeData(
     val name : String,
     val description : String,
     val base : String
