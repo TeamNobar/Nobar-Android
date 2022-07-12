e74163847c7074ec1ad3e3081f1b6ef29fe1da65
hyeonjeong Park <py0429@ewhain.net>
2022-07-12 18:56:56 +0900

    #12 디자인 수정 적용

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/recipe/RecipeActivity.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/recipe/RecipeActivity.kt
@@ -6,14 +6,10 @@ import org.sopt.appzam.nobar_android.databinding.ActivityRecipeBinding
 
 class RecipeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecipeBinding
-    private lateinit var adapter: RecipeAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecipeBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    private fun initAdapter() {
-        adapter = RecipeAdapter()
-    }
 }
