02981ee505c857017c117de9478589f25425443c
hyeonjeong Park <py0429@ewhain.net>
2022-07-13 19:24:55 +0900

    #17 main 초기 화면 추가, myPage later recipe item 생성

--- app/src/main/java/org/sopt/appzam/nobar_android/data/remote/response/MyPageResponse.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/data/remote/response/MyPageResponse.kt
@@ -2,6 +2,6 @@ package org.sopt.appzam.nobar_android.data.remote.response
 
 data class MyPageResponse(
     val nickName : String,
-    val laterRecipes : List<String>,
-    val tastingNotes : List<String>
+    val laterRecipes : List<MyPageLaterRecipeResponse>,
+    val tastingNotes : List<MyPageTastingResponse>
 )
--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/mypage/MyPageLaterRecipeFragment.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/mypage/MyPageLaterRecipeFragment.kt
@@ -5,13 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.tabs.TabLayoutMediator
 import org.sopt.appzam.nobar_android.R
-import org.sopt.appzam.nobar_android.databinding.FragmentMypageBinding
+import org.sopt.appzam.nobar_android.databinding.FragmentMyPageLaterRecipeBinding
 import org.sopt.appzam.nobar_android.presentation.base.BaseFragment
 
-class MypageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
-
+class MyPageLaterRecipeFragment : BaseFragment<FragmentMyPageLaterRecipeBinding>(R.layout.fragment_my_page_later_recipe) {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,16 +18,4 @@ class MypageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_myp
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        initViewPager()
-    }
-
-    private fun initViewPager(){
-        val fragmentList = listOf(MyPageLaterRecipeFragment(), MyPageTastingFragment())
-
-
-
-    }
 }
