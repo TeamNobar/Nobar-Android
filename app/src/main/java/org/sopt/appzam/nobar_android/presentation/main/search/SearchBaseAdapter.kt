3d495c2fa1b7007b1b663e2e5835a1a4432b9378
권용민 <briandr@naver.com>
2022-07-15 18:29:12 +0900

    make SearchBaseAdapter.kt

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/search/SearchBaseAdapter.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/search/SearchBaseAdapter.kt
@@ -1,15 +1,43 @@
 package org.sopt.appzam.nobar_android.presentation.main.search
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import org.sopt.appzam.nobar_android.data.remote.response.IngredientResponse
+import com.bumptech.glide.Glide
+import org.sopt.appzam.nobar_android.data.remote.response.common.BaseResponse
 import org.sopt.appzam.nobar_android.databinding.ItemSearchBaseBinding
 
-class SearchBaseAdapter : ListAdapter<> {
+class SearchBaseAdapter :
+    ListAdapter<BaseResponse, SearchBaseAdapter.SearchBaseViewHolder>(SearchBaseComparator()) {
 
-    class SearchBaseViewHolder(private val binding : ItemSearchBaseBinding) : RecyclerView.ViewHolder(binding.root){
-        fun onBind(data : IngredientResponse){
-
+    class SearchBaseViewHolder(private val binding: ItemSearchBaseBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: BaseResponse) {
+            binding.model = data
+            Glide.with(binding.imageBase).load(data.url).into(binding.imageBase)
         }
+    }
+
+    class SearchBaseComparator : DiffUtil.ItemCallback<BaseResponse>() {
+        override fun areItemsTheSame(oldItem: BaseResponse, newItem: BaseResponse): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: BaseResponse, newItem: BaseResponse): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchBaseViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemSearchBaseBinding.inflate(layoutInflater, parent, false)
+        return SearchBaseViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchBaseViewHolder, position: Int) {
+        holder.onBind(getItem(position))
     }
 }
