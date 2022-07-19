ecc438bece0836d78efd637bc70ddddaabfc6fe2
권용민 <briandr@naver.com>
2022-07-19 16:02:08 +0900

    modify base data

---
+++ app/src/main/java/org/sopt/appzam/nobar_android/data/remote/response/BaseResponse.kt
@@ -0,0 +1,7 @@
+package org.sopt.appzam.nobar_android.data.remote.response
+
+import org.sopt.appzam.nobar_android.data.remote.response.common.BaseModel
+
+data class BaseResponse(
+    val base : List<BaseModel>
+)
