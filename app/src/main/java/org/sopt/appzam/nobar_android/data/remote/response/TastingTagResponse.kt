f7a4673c12211fcc8cd11e180e858ac5af774aef
hyeonjeong Park <py0429@ewhain.net>
2022-07-20 23:55:44 +0900

    #37 myPage tasting response 수정

---
+++ app/src/main/java/org/sopt/appzam/nobar_android/data/remote/response/TastingTagResponse.kt
@@ -0,0 +1,8 @@
+package org.sopt.appzam.nobar_android.data.remote.response
+
+data class TastingTagResponse(
+    val id : String,
+    val category : Int,
+    val content : String,
+    val isSelected : Boolean
+)
