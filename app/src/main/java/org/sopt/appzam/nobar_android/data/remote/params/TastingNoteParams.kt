06185437b1fe3719c721352c9996978457cdb401
권용민 <briandr@naver.com>
2022-07-22 05:26:55 +0900

    make TastingNoteParams.kt

---
+++ app/src/main/java/org/sopt/appzam/nobar_android/data/remote/params/TastingNoteParams.kt
@@ -0,0 +1,12 @@
+package org.sopt.appzam.nobar_android.data.remote.params
+
+import org.sopt.appzam.nobar_android.data.remote.response.TagResponse
+
+data class TastingNoteParams(
+    val recipeId : String,
+    val rate : Double,
+    val tagList : List<TagResponse>,
+    val tasteContent : String?,
+    val experienceContent : String?,
+    val createdAt : String
+)
