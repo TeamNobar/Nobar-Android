f529c9b3f5447ffb0362d2c626a4c7313901dff0
hyeonjeong Park <py0429@ewhain.net>
2022-07-22 03:05:11 +0900

    #44 home nobar recipe 클릭시 이동

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/home/NobarRecipeAdapter.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/home/NobarRecipeAdapter.kt
@@ -7,13 +7,20 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.appzam.nobar_android.R
+import org.sopt.appzam.nobar_android.data.remote.response.GuideResponse
 import org.sopt.appzam.nobar_android.data.remote.response.NobarRecipeResponse
 import org.sopt.appzam.nobar_android.databinding.ItemHomeNobarRecipeBinding
 
-class NobarRecipeAdapter(private val randomIntList: List<Int>) :
+class NobarRecipeAdapter(
+    private val randomIntList: List<Int>,
+    private val itemClick: (NobarRecipeResponse) -> (Unit)
+) :
     ListAdapter<NobarRecipeResponse, NobarRecipeAdapter.NobarRecipeViewHolder>(NobarRecipeComparator()) {
 
-    class NobarRecipeViewHolder(private val binding: ItemHomeNobarRecipeBinding) :
+    class NobarRecipeViewHolder(
+        private val binding: ItemHomeNobarRecipeBinding,
+        private val itemClick: (NobarRecipeResponse) -> Unit
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: NobarRecipeResponse, colorInt: Int) {
             binding.nobarRecipeItem = data
@@ -24,17 +31,20 @@ class NobarRecipeAdapter(private val randomIntList: List<Int>) :
                 3 -> binding.layout.setBackgroundResource(R.drawable.home_navy_box4)
                 4 -> binding.layout.setBackgroundResource(R.drawable.home_navy_box5)
             }
+            binding.root.setOnClickListener {
+                itemClick(data)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NobarRecipeViewHolder {
         val binding =
             ItemHomeNobarRecipeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return NobarRecipeViewHolder(binding)
+        return NobarRecipeViewHolder(binding, itemClick)
     }
 
     override fun onBindViewHolder(holder: NobarRecipeViewHolder, position: Int) {
-        Log.d("안녕",position.toString())
+        Log.d("안녕", position.toString())
         holder.onBind(getItem(position), randomIntList[position])
     }
 
