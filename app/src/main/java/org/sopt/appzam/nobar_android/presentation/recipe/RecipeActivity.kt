4bea30455623d740644375d54362b7838d08755a
권용민 <briandr@naver.com>
2022-07-22 00:36:53 +0900

    complete recipe ingredients recyclerview

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/recipe/RecipeActivity.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/recipe/RecipeActivity.kt
@@ -10,12 +10,15 @@ import org.sopt.appzam.nobar_android.presentation.base.BaseActivity
 class RecipeActivity : BaseActivity<ActivityRecipeBinding>(R.layout.activity_recipe) {
     private val recipeViewModel : RecipeViewModel by viewModels()
     private lateinit var recipeCoreInfoAdapter : RecipeCoreInfoAdapter
+    private lateinit var recipeIngredientAdapter: RecipeIngredientAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getRecipeDetailData()
         initRecipeCoreInfoAdapter()
-        observingCoreData()
+        initCoreData()
+        initIngredientData()
+        initRecipeIngredientAdapter()
     }
 
     private fun initRecipeCoreInfoAdapter(){
@@ -23,9 +26,20 @@ class RecipeActivity : BaseActivity<ActivityRecipeBinding>(R.layout.activity_rec
         binding.recyclerCoreInfo.adapter=recipeCoreInfoAdapter
     }
 
-    private fun observingCoreData(){
+    private fun initRecipeIngredientAdapter(){
+        recipeIngredientAdapter = RecipeIngredientAdapter()
+        binding.recyclerIngredients.adapter=recipeIngredientAdapter
+    }
+
+    private fun initCoreData(){
         recipeViewModel.recipeCoreInfo.observe(this){
             recipeCoreInfoAdapter.submitList(it)
+        }
+    }
+
+    private fun initIngredientData(){
+        recipeViewModel.recipeDetail.observe(this){
+            recipeIngredientAdapter.submitList(it.ingredients)
         }
     }
 
