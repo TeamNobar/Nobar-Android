049e97341f4923d1881c6731ee01db25b12eb71e
권용민 <briandr@naver.com>
2022-07-22 05:38:20 +0900

    add getTagListNetwork in viewModel

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/record/RecordViewModel.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/record/RecordViewModel.kt
@@ -1,7 +1,23 @@
 package org.sopt.appzam.nobar_android.presentation.main.record
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import org.sopt.appzam.nobar_android.data.remote.api.ServiceCreator
+import org.sopt.appzam.nobar_android.data.remote.response.TagResponse
 import org.sopt.appzam.nobar_android.presentation.base.NobarViewModel
+import org.sopt.appzam.nobar_android.util.enqueueUtil
+import retrofit2.Call
 
 class RecordViewModel() : NobarViewModel() {
+    private var _tagList = MutableLiveData<List<TagResponse>>()
+    val tagList: LiveData<List<TagResponse>> = _tagList
 
+    fun getTagListNetwork() {
+        val call: Call<List<TagResponse>> = ServiceCreator.mockupService.getTagList()
+        call.enqueueUtil(
+            onSuccess = { _tagList.value=it },
+            onError = { Log.d("server", "오류") }
+        )
+    }
 }
