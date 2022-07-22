ab24d3f07082a6868421f8f30d4c569cb2fd76c6
권용민 <briandr@naver.com>
2022-07-23 07:53:24 +0900

    add date, rating to ReadNote

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/record/RecordReadFragment.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/record/RecordReadFragment.kt
@@ -13,12 +13,32 @@ class RecordReadFragment : BaseFragment<FragmentRecordReadBinding>(R.layout.frag
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewmodel=recordViewModel
+        binding.viewmodel = recordViewModel
         initAdapter()
+        putDate()
+        putRating()
     }
 
     private fun initAdapter() {
-        tagAdapter = RecordTagAdapter{ }
+        tagAdapter = RecordTagAdapter { }
         binding.recyclerView.adapter = tagAdapter
+    }
+
+    private fun putDate() {
+        recordViewModel.drinkingDate.observe(viewLifecycleOwner) {
+            val yearRange = IntRange(0, 3)
+            val monthRange = IntRange(5, 6)
+            val dayRange = IntRange(8, 9)
+            val year = it.slice(yearRange).toInt()
+            val month = it.slice(monthRange).toInt()
+            val day = it.slice(dayRange).toInt()
+            binding.textWhen.text = String.format("%d년 %2d월 %2d일", year, month, day)
+        }
+    }
+
+    private fun putRating(){
+        recordViewModel.rating.observe(viewLifecycleOwner){
+            binding.rotationRatingBar.rating=it.toFloat()
+        }
     }
 }
