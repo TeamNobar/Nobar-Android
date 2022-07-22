37bb7be23707b06b74499eae771749d7fcc93aab
권용민 <briandr@naver.com>
2022-07-23 06:43:49 +0900

    X버튼 추가 및 activity result 추가

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/record/RecordWritingFragment.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/record/RecordWritingFragment.kt
@@ -1,5 +1,6 @@
 package org.sopt.appzam.nobar_android.presentation.main.record
 
+import android.app.Activity.RESULT_CANCELED
 import android.app.Activity.RESULT_OK
 import android.app.DatePickerDialog
 import android.content.Intent
@@ -13,6 +14,7 @@ import org.sopt.appzam.nobar_android.R
 import org.sopt.appzam.nobar_android.data.remote.response.TagResponse
 import org.sopt.appzam.nobar_android.databinding.FragmentRecordWritingBinding
 import org.sopt.appzam.nobar_android.presentation.base.BaseFragment
+import org.sopt.appzam.nobar_android.presentation.main.mypage.MyPageTastingFragment
 import org.sopt.appzam.nobar_android.presentation.main.search.SearchDetailActivity
 import org.sopt.appzam.nobar_android.presentation.main.search.SearchDetailActivity.Companion.FROM
 import org.sopt.appzam.nobar_android.presentation.main.search.SearchDetailActivity.Companion.NOTE
@@ -47,6 +49,9 @@ class RecordWritingFragment :
 
         //글쓰기 등록 버튼
         clickEnrollment(binding.rotationRatingBar.rating)
+
+        //종료
+        clickX()
     }
 
     private fun initTagAdapter() {
@@ -120,6 +125,17 @@ class RecordWritingFragment :
     private fun clickEnrollment(rating: Float) {
         binding.textEnrollment.setOnClickListener {
             recordViewModel.postTastingNote(recordViewModel.makeTastingNote(rating))
+            val intent = Intent(requireContext(), MyPageTastingFragment::class.java)
+            activity?.setResult(RESULT_OK, intent)
+            activity?.finish()
+        }
+    }
+
+    private fun clickX() {
+        binding.imageX.setOnClickListener {
+            val intent = Intent(requireContext(), MyPageTastingFragment::class.java)
+            activity?.setResult(RESULT_CANCELED, intent)
+            activity?.finish()
         }
     }
 
