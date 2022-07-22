75ea5c6d8f8baad407b7dc4a22819787dbad6ad9
권용민 <briandr@naver.com>
2022-07-22 11:07:45 +0900

    modify response data class

--- app/src/main/java/org/sopt/appzam/nobar_android/data/remote/response/TagResponse.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/data/remote/response/TagResponse.kt
@@ -3,5 +3,6 @@ package org.sopt.appzam.nobar_android.data.remote.response
 data class TagResponse(
     val id : String,
     val content : String,
+    val icon : String,
     val isSelected : Boolean
 )
