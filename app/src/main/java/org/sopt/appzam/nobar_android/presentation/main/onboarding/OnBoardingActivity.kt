ad150553282f7f96f17c08805e6e0997be5aeea2
hyeonjeong Park <py0429@ewhain.net>
2022-07-22 16:48:30 +0900

    #49 onboarding activity

---
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.appzam.nobar_android.presentation.main.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.appzam.nobar_android.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
---
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/onboarding/OnBoardingFragment3.kt
@@ -0,0 +1,21 @@
+package org.sopt.appzam.nobar_android.presentation.main.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.sopt.appzam.nobar_android.R
+
+
+class OnBoardingFragment3 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_on_boarding3, container, false)
+    }
+
+}
