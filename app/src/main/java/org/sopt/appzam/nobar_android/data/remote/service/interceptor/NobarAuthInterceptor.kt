532daba9e00366bdceb19f16215a95561f795402
MalibinYun <nightmare73@naver.com>
2022-07-23 00:44:47 +0900

    터지던 이슈 해결 및 Interceptor 원복

--- app/src/main/java/org/sopt/appzam/nobar_android/data/remote/service/interceptor/NobarAuthInterceptor.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/data/remote/service/interceptor/NobarAuthInterceptor.kt
@@ -7,12 +7,10 @@ import org.sopt.appzam.nobar_android.data.local.db.AuthTokenManager
 class NobarAuthInterceptor(
         private val nobarTokenManager: AuthTokenManager,
 ) : Interceptor {
-
     override fun intercept(chain: Interceptor.Chain): Response {
-        /*val nobarToken = nobarTokenManager.getNobarToken()?.accessToken
-                ?: return chain.proceed(chain.request())*/
+        val nobarToken = nobarTokenManager.getNobarToken()?.accessToken
+                ?: return chain.proceed(chain.request())
 
-        val nobarToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkOWRkYmFkYmI1NDBiODhjZDVlZGM4In0sImlhdCI6MTY1ODQ3NjAwMCwiZXhwIjo0NzgyNjc4NDAwfQ.PDvodnxCJrq8XTxoZIcYdNheK9FSB-wjjfe2_t8-D-Q"
         val tokenAddedRequest = chain.request().newBuilder()
                 .addHeader(TOKEN, nobarToken)
                 .build()
