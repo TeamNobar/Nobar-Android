12efc05ea09c1ccc6efc0b9a8d89c982631820b1
권용민 <briandr@naver.com>
2022-07-23 06:44:10 +0900

    activity result 추가

--- app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/mypage/MyPageTastingFragment.kt
+++ app/src/main/java/org/sopt/appzam/nobar_android/presentation/main/mypage/MyPageTastingFragment.kt
@@ -1,10 +1,13 @@
 package org.sopt.appzam.nobar_android.presentation.main.mypage
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import org.sopt.appzam.nobar_android.R
 import org.sopt.appzam.nobar_android.databinding.FragmentMyPageTastingBinding
@@ -18,27 +21,43 @@ class MyPageTastingFragment :
     BaseFragment<FragmentMyPageTastingBinding>(R.layout.fragment_my_page_tasting) {
     private lateinit var multiViewAdapter: MultiViewAdapter
     private val myPageViewModel by viewModels<MyPageViewModel>()
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-
         binding.myPageTastingViewModel = myPageViewModel
-        initNetwork()
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        tastingNoteAdapter()
+        initObserver()
+        initNetwork()
+
+        clickWritingNote()
+        setWritingResult()
+    }
+
+    private fun clickWritingNote() {
         binding.buttonNewNote.setOnClickListener {
             val intent = Intent(requireActivity(), RecordActivity::class.java)
             intent.putExtra(MODE, NEW)
-            startActivity(intent)
+            resultLauncher.launch(intent)
         }
-        tastingNoteAdapter()
-        initObserver()
+    }
+
+    private fun setWritingResult() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    initNetwork()
+                    initObserver()
+                }
+            }
     }
 
     private fun tastingNoteAdapter() {
@@ -54,7 +73,5 @@ class MyPageTastingFragment :
         myPageViewModel.tastingNotes.observe(viewLifecycleOwner) {
             multiViewAdapter.submitList(it)
         }
-
     }
-
 }
